15e492af0eb98ac9ac13dba481f80ad66fb8f6f7
hali <hali@moonjoy.net>
2017-11-24 11:24:53 +0700

    install jwt

--- config/app.php
+++ config/app.php
@@ -177,6 +177,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        //jwt
+        //Tymon\JWTAuth\Providers\JWTAuthServiceProvider::class//旧版
+        Tymon\JWTAuth\Providers\LaravelServiceProvider::class
+
     ],
 
     /*
@@ -225,6 +229,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        //jwt
+        'JWTAuth' => Tymon\JWTAuth\Facades\JWTAuth::class,
+        'JWTFactory' => Tymon\JWTAuth\Facades\JWTFactory::class
 
     ],
 

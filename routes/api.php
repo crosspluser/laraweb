93c91d3d1247a2cf3ecb81218667eb9fdf23f689
hali <hali@moonjoy.net>
2017-12-20 18:47:46 +0700

    1

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,30 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//获取请求：1、自动注入 服务容器；2、路由参数 在 服务容器 之后；
+Route::any( '/request', function( Request $request ){
+
+    return [
+        //请求属性：路径、方法等
+        'path'     => $request->path(),
+        'url'      => $request->url(),
+        'fullUrl'  => $request->fullUrl(),
+        'method'   => $request->method(),
+        'is'       => $request->is( 'reQuest' ),//路由判断：区分大小写
+        'isMethod' => $request->isMethod( 'GeT' ),//方法判断：不区分大小写
+        //预处理&规范化
+        'all' => $request->all(),//参数
+        'query' => $request->query(),//查询：只获取跟随url提交的query，不获取表单等形式提交的；
+        'param_default' => $request->input('param_default','param_default'),//参数
+        'array' => $request->input('array.*'),//数组：数字取偏移；*号取全部
+        'temp' => $request->temp,//动态：先请求数据，再路由参数
+        'json' => $request->input('key'),//json：自动解析成对象；标准json格式应该用双引号
+        'only'=>$request->only(['temp']),
+        'except'=>$request->except(['temp']),
+        'has'=>$request->has('has','temp'),
+        'filled'=>$request->filled('filled','has'),
+    ];
+} )->name('');
+
+//PSR-7 请求：一种更开放的接口标准，用到再研究
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,34 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+use Illuminate\Http\Request;
+
+Route::get( '/', function(){
+    return view( 'welcome' );
+} );
+
+//获取旧输入
+Route::get( '/flash', function( Request $request ){
+    //旧输入
+    //$request->flash();
+    //$request->flashOnly('flashOnly');
+    //$request->flashExcept('flashExcept');
+
+    return [
+        //请求属性：路径、方法等
+        'fullUrl'  => $request->fullUrl(),
+        'method'   => $request->method(),
+        //预处理&规范化
+        'all' => $request->all(),//参数
+        'input' => $request->input(),
+        //旧输入
+        'old'=>$request->old()
+    ];
+} )->name('flash');
+
+//请求重定向
+Route::get( '/request-redirect', function( Request $request ){
+    return redirect('flash')->withInput(
+        $request->except('except')//转发参数
+    );
+} );

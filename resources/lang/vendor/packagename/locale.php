3fe245379a266165de48b60bafd00403ed8a8dee
hali <hali@moonjoy.net>
2018-01-09 09:36:35 +0700

    5.5_localization

--- resources/lang/vendor/packagename/locale.php
+++ resources/lang/vendor/packagename/locale.php
@@ -1,7 +1,7 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: hali
- * Date: 2018/1/8
- * Time: 17:29
- */
+return [
+    //重写扩展包的语言文件
+    /**
+     * 部分扩展包可能会附带自己的语言文件。你可以通过在 resources/lang/vendor/{package}/{locale} 放置文件来重写它们，而不是直接修改扩展包的核心文件
+     */
+];
--- resources/lang/vendor/packagename/messages.php
+++ resources/lang/vendor/packagename/messages.php
@@ -1,7 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: hali
- * Date: 2018/1/8
- * Time: 17:29
- */
+return [
+    //重写扩展包的语言文件
+    /**
+     * 例如，你需要重写 skyrim/hearthfire 扩展包的英文语言文件 messages.php ，则需要把文件放置在 resources/lang/vendor/hearthfire/en/messages.php 。在这个文件中只应定义要覆盖的翻译字符串。任何没有被覆盖的翻译字符串仍将从扩展包的原始语言文件中加载。
+     */
+    //注意文件名保持相同
+];
